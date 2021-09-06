6504cfbc3ca9ea187f220b725d892b642fa34888
Andrés Nuñez <andresanunez98@gmail.com>
2021-09-06 18:18:38 -0400

    styling changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import React from "react";
 const Home = () => {
   return (
     <div className="centered">
-      <h5>
+      <p className="home-paragraph">
         {" "}
         Welcome to OnTrak: A virtual way for you to log the last time you took
         your medication. Begin by adding a medication to your list.
-      </h5>
+      </p>
     </div>
   );
 };
